e6025e0d2761702e2f6d33fe3b7ada9a67c5df76
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-08-28 20:44:12 -0600

    Initial commit

---
+++ SimplifyDes.java
@@ -0,0 +1,26 @@
+public class SimplifyDes{
+    public static void main(String[] args){
+        return;
+    }
+}
+/*
+public byte [4][4] s0 =
+public byte [4][4] s1 =
+
+Bit[8] getKey(Bit[10] key,byte offset){
+    Bit[10] adas = {key[1],key[3],key[9]...}
+    return key;
+}
+Bit[8] S-DES(Bit[8] plain, key1, key2){
+    return msg;
+}
+Bit[] xOR(Bit[] arr1, Bit[] arr2){
+    Hacer XOR de arr1 con arr2
+    return arr3;
+}
+Bit[2] Sbox(Bit[4] arr,byte[][] s){
+    hacer lo de las cajitas
+    return newArr;
+}
+*/
+//Cindy7td
